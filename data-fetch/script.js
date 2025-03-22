61876681b1f7ca6e332f89347580f006deb1654f
naveen-b26 <naveenbandari370@gmail.com>
2025-03-23 00:00:40 +0530

    Update script.js

--- data-fetch/script.js
+++ data-fetch/script.js
@@ -18,7 +18,7 @@ async function fetchUsers() {
     }
 
     try {
-        const response = await fetch("http://localhost:5000/api/users", {
+        const response = await fetch("https://nonucure-bot.vercel.app/api/users", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
