5556efcdec766826fe653fe8dafbf629378fda66
naveen-b26 <naveenbandari370@gmail.com>
2025-05-09 14:28:40 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,11 @@ const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://nonucare-bot.vercel.app', // your frontend's domain
+  methods: ['POST', 'GET'], // adjust as needed
+  credentials: true
+}));
 app.use(express.json());
 
 // Default route
