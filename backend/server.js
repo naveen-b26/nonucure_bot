b0b9d6e35b5e1a2babab03109a4452f6d8ff85c7
naveen-b26 <naveenbandari370@gmail.com>
2025-05-13 16:12:46 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,13 @@ const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 
 // Middleware
-app.use(cors());
+app.use(cors(
+  {
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  }
+));
 app.use(express.json());
 
 // Default route
